202abfa6a8385962f9b38fa6815155cbf9f2c344
miuser <miuser@email.com>
2022-07-18 17:05:55 +0800

    init

---
+++ src/main/java/com/github/makewheels/solarwaterserver/SolarWaterServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.solarwaterserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarWaterServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolarWaterServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/makewheels/solarwaterserver/SolarWaterServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.solarwaterserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarWaterServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
