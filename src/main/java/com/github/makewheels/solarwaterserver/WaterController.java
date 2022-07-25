f0dec00c7bf66d522ec508b7e3d7fd97d02093f9
miuser <miuser@email.com>
2022-07-25 23:28:08 +0800

    上线

---
+++ src/main/java/com/github/makewheels/solarwaterserver/WaterController.java
@@ -0,0 +1,18 @@
+package com.github.makewheels.solarwaterserver;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class WaterController {
+    @Resource
+    private WaterService waterService;
+
+    @GetMapping("connect")
+    public String connect(@RequestParam long timeLength) {
+        return waterService.connect(timeLength);
+    }
+}
--- src/main/java/com/github/makewheels/solarwaterserver/WaterService.java
+++ src/main/java/com/github/makewheels/solarwaterserver/WaterService.java
@@ -1,47 +1,52 @@
 package com.github.makewheels.solarwaterserver;
 
 import cn.hutool.core.codec.Base64;
-import cn.hutool.core.util.IdUtil;
 import com.alibaba.fastjson2.JSON;
 import com.aliyun.iot20180120.Client;
 import com.aliyun.iot20180120.models.PubRequest;
 import com.aliyun.iot20180120.models.PubResponse;
 import com.aliyun.teaopenapi.models.Config;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Service
+@Slf4j
 public class WaterService {
+    private Client aliyunClient;
 
-    public static Client createClient(String accessKeyId, String accessKeySecret)
-            throws Exception {
+    private Client getClient() throws Exception {
+        if (aliyunClient != null) {
+            return aliyunClient;
+        }
         Config config = new Config()
-                // 您的 AccessKey ID
-                .setAccessKeyId(accessKeyId)
-                // 您的 AccessKey Secret
-                .setAccessKeySecret(accessKeySecret);
+                .setAccessKeyId(Base64.decodeStr("TFRBSTV0RDZIajRpNkQ4Q3h6Qmh1Zktr"))
+                .setAccessKeySecret(Base64.decodeStr(
+                        "d2NaMXIwNVhpc2NZSER4cGo5ZklScjN4eFVIcEFX"));
         config.endpoint = "iot.cn-shanghai.aliyuncs.com";
-        return new Client(config);
+        aliyunClient = new Client(config);
+        return aliyunClient;
     }
 
-    public static void main(String[] args) {
-        Client client;
-        try {
-            client = WaterService.createClient(
-                    Base64.decodeStr("TFRBSTV0RDZIajRpNkQ4Q3h6Qmh1Zktr"),
-                    Base64.decodeStr("d2NaMXIwNVhpc2NZSER4cGo5ZklScjN4eFVIcEFX"));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        String content = "2000";
+    private void doAliyunConnect(String content) {
         PubRequest pubRequest = new PubRequest()
                 .setProductKey("a1g0aXwTsx1")
                 .setTopicFullName("/a1g0aXwTsx1/ZApjdvupCbBZlbJYFILD/user/get")
                 .setMessageContent(Base64.encode(content));
         PubResponse pubResponse;
         try {
-            pubResponse = client.pub(pubRequest);
+            pubResponse = getClient().pub(pubRequest);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
         System.out.println(JSON.toJSONString(pubResponse));
     }
+
+
+    public String connect(long timeLength) {
+        doAliyunConnect(timeLength + "");
+        log.info("链接: " + timeLength);
+        return "response-" + System.currentTimeMillis();
+    }
+
 }
