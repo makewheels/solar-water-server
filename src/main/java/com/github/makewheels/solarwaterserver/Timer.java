ac9b97c2c983d627adb039ec917d321017372c27
diypcfev <finalbird@foxmail.com>
2022-08-03 09:31:51 +0800

    冬天防冻代码准备

--- src/main/java/com/github/makewheels/solarwaterserver/Timer.java
+++ src/main/java/com/github/makewheels/solarwaterserver/Timer.java
@@ -10,14 +10,22 @@ public class Timer {
     @Resource
     private WaterService waterService;
 
+    //夏天定时上水1
     @Scheduled(cron = "0 0 4 * * ?")
     public void task1() {
         waterService.connect(1000 * 60 * 10);
     }
 
+    //夏天定时上水2
     @Scheduled(cron = "0 2 12 * * ?")
     public void task2() {
         waterService.connect(1000 * 60 * 5);
     }
+
+    //冬天防冻顶水
+//    @Scheduled(cron = "0 1 21-23,0-7 * * ?")
+//    public void task3() {
+//        waterService.connect(1000 * 10);
+//    }
 
 }
