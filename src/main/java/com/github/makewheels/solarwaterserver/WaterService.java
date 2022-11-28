e0b6d04b00b436514057debbf1c76c1a91b53abb
diypcfev <33256842+makewheels@users.noreply.github.com>
2022-11-28 18:13:37 +0800

    fastjson改到1版本

--- src/main/java/com/github/makewheels/solarwaterserver/WaterService.java
+++ src/main/java/com/github/makewheels/solarwaterserver/WaterService.java
@@ -1,7 +1,7 @@
 package com.github.makewheels.solarwaterserver;
 
 import cn.hutool.core.codec.Base64;
-import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson.JSON;
 import com.aliyun.iot20180120.Client;
 import com.aliyun.iot20180120.models.PubRequest;
 import com.aliyun.iot20180120.models.PubResponse;
